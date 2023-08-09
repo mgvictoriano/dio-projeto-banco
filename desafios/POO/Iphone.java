223dec9bc80868b96efa3f3e2d60c01bc7d6919a
Michelle Gaia Victoriano <mgvictoriano@live.com>
2023-08-09 15:52:33 +0000

    implementando classes e interfaces de Iphone

--- desafios/POO/Iphone.java
+++ desafios/POO/Iphone.java
@@ -1,3 +1,52 @@
-public class Iphone {
+import interfaces.AparelhoTelefonico;
+import interfaces.NavegadorInternet;
+import interfaces.ReprodutorMusical;
+
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void tocar() {
+        // Implementação do método tocar
+    }
+
+    @Override
+    public void pausar() {
+        // Implementação do método pausar
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        // Implementação do método selecionarMusica
+    }
+
+    @Override
+    public void ligar(String numero) {
+        // Implementação do método ligar
+    }
+
+    @Override
+    public void atender() {
+        // Implementação do método atender
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        // Implementação do método iniciarCorreioVoz
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        // Implementação do método exibirPagina
+    }
+
+    @Override
+    public void adicionarNovaAba(String url) {
+        // Implementação do método adicionarNovaAba
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // Implementação do método atualizarPagina
+    }
     
 }
---
+++ desafios/POO/interfaces/ReprodutorMusical.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface ReprodutorMusical {
+    void tocar();
+    void pausar();
+    void selecionarMusica(String musica);
+}
